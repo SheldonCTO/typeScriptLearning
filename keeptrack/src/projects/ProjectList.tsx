53345becd4a999ae72f54508480d3f077fdaeb05
SZETO Ngai man <c.cto1981@gmail.com>
2024-09-17 14:14:47 -0400

    hideAndShow

--- keeptrack/src/projects/ProjectList.tsx
+++ keeptrack/src/projects/ProjectList.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
 
 interface ProjectListProps {
-  projects: Project[];
+	projects: Project[];
 }
 
 function ProjectList({ projects }: ProjectListProps) {
+	const [projectBeingEdited, setProjectBeingEdited] = useState({});
+	const handleEdit = (project: Project) => {
+		// console.log(project);
+		setProjectBeingEdited(project);
+	};
+
 	return (
-	    <div className="row">
-	      {projects.map((project) => (
-	        <div key={project.id} className="cols-sm">
-	          <ProjectCard project={project}></ProjectCard>
-			  <ProjectForm />
-	        </div>
-	      ))}
-	    </div>
+		<div className="row">
+			{projects.map((project) => (
+				<div key={project.id} className="cols-sm">
+					{project === projectBeingEdited ? (
+						<ProjectForm />
+					) : (
+						<ProjectCard project={project} onEdit={handleEdit} />
+					)}
+				</div>
+			))}
+		</div>
 	);
 }
 
