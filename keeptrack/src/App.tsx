d68fde9ade10790a16cca919e0b1572e5aca1bb0
SZETO Ngai man <c.cto1981@gmail.com>
2024-09-21 10:26:53 -0400

    nav bar

--- keeptrack/src/App.tsx
+++ keeptrack/src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import ProjectsPage from './projects/ProjectsPage';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import HomePage from './home/HomePage';
 
 function App() {
   return (
-    <div className="container">
-      <ProjectsPage />
-  </div>
+    <Router>
+      {/* nav bar setup and see css*/}
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        {/* nav bar button */}
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
